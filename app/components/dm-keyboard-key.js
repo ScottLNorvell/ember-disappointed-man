bb6fc57900f7cef84af3db21f946715e729fca40
Scott Norvell <scottlnorvell@gmail.com>
2015-11-29 17:14:17 -0500

    disable buttons properly...

--- app/components/dm-keyboard-key.js
+++ app/components/dm-keyboard-key.js
@@ -10,15 +10,16 @@ const {
 export default Component.extend({
   classNames: ['dm-keyboard-key'],
   classNameBindings: ['guessed'],
-  attributeBindings: ['disabled'],
-  disabled: computed('guessed', 'guess.gameOver', function() {
-    return isPresent(this.get('guessed')) || this.get('guess.gameOver');
+  disabled: computed('guessed', 'gameComplete', function() {
+    return isPresent(this.get('guessed')) || this.get('gameComplete');
   }),
   guess: inject.service(),
+  gameComplete: computed.or('guess.gameOver', 'guess.success'),
   letter: computed.alias('key.key'),
   guessed: computed.alias('key.guessed'),
+
   click() {
-    if (isPresent(this.get('guessed'))) { return; }
+    if (this.get('disabled')) { return; }
     let goodGuess = this.get('guess').guess(this.get('letter'));
     let guessed = goodGuess ? 'correct' : 'incorrect';
     this.set('guessed', guessed);
